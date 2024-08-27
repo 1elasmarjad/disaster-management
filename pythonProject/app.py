ec63ae4da67fe25eb2cd3df8f5c3c6d77c63849b
1elasmarjad <jadelasmar4@gmail.com>
2024-08-27 00:28:31 -0400

    🌍 Fetching from usgs.gov for earthquake data

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,14 +1,32 @@
+from datetime import datetime, timedelta
+
 from flask import Flask
+import requests
+import urllib.parse
+
+from proj_types.earthquake_api import EarthquakeFetchData
 
 app = Flask(__name__)
 
+DATA_FRESHNESS = timedelta(days=7)
 
-@app.route('/')
-def hello_word():
-    return {
-        'message': 'Hello, World!',
-        "test": "test"
+
+@app.route('/api/data')
+def fetch_api_data():
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+
+    current_date = datetime.now()
+    start_time = current_date - DATA_FRESHNESS
+
+    params = {
+        "format": "geojson",
+        "starttime": start_time.strftime("%Y-%m-%dT%H:%M:%S"),
     }
+
+    resp = requests.get(url + "?" + urllib.parse.urlencode(params))
+    data: EarthquakeFetchData = resp.json()
+
+    return data
 
 
 if __name__ == '__main__':
---
+++ pythonProject/proj_types/earthquake_api.py
@@ -0,0 +1,29 @@
+from typing import TypedDict, Literal
+
+
+class FeatureGeometry(TypedDict):
+    type: Literal["Point"]
+    coordinates: list[float]
+
+
+class FeatureProperties(TypedDict):
+    # note: excludes many unused properties
+    mag: float
+    time: int
+    type: Literal["earthquake"]
+    place: str
+    updated: int
+    url: str
+    detail: str
+    code: str
+
+
+class EarthquakeFeature(TypedDict):
+    geometry: FeatureGeometry
+    id: str
+    type: Literal["Feature"]
+
+
+class EarthquakeFetchData(TypedDict):
+    bbox: list[float]
+    features: list[EarthquakeFeature]
